8ee5873cd59c5c4d8eb65cf27399a132c7fef55c
Firuza <firusacolombina@gmail.com>
2023-03-06 17:58:31 +0100

    cleaned code of SVG, removed localhost setup, added redirection instead, added methods to be able to play against AI, with adjusted pics showing

---
+++ RPS/FifthPage/FifthPage.js
@@ -0,0 +1,24 @@
+const rockButton = document.getElementById("rock")
+rockButton.addEventListener('click', function () { choiceTaker(0) })
+
+const paperButton = document.getElementById("paper")
+paperButton.addEventListener('click', function () { choiceTaker(1) })
+
+const scissorsButton = document.getElementById("scissors")
+scissorsButton.addEventListener('click', function () { choiceTaker(2) })
+
+
+function choiceTaker(userInput) {
+    let aiChoice = Math.floor(Math.random() * 3);
+
+   
+    let url = "../SixthPage/SixthPage.html"
+        + "?_ijt=rmgf90662bv8lfqsd2r74lb2uc"
+        + "&_ij_reload=RELOAD_ON_SAVE"
+        + "&player=" + userInput
+        + "&opponent=" + aiChoice;
+
+    window.location.href = url;
+
+
+}
